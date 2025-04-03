85af9f51c07b2134d03af38e1fa97e82c9db5301
Camila <85712574004@senacrs.edu.br>
2025-04-03 11:23:29 -0300

    atualização

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const resultado = document.querySelector ('#resultado')
-const altura = document.querySelector ('#altura')
-const peso = document.querySelector ('#peso')
+let resultado = document.querySelector('#resultado');
+let altura = document.querySelector('#altura');
+let peso = document.querySelector('#peso');
+
+function calculoimc(){
+    if (altura.value !== '' && peso.value !== '') {
+        const imc = (peso.value / (altura.value * altura.value)). toFixed(2)
+        let classification =''
+        if (imc < 18.5){
+            classification = 'Abaixo do peso'
+        } else if (imc >= 18.5 && imc < 25){
+            classification = 'Peso normal'
+        } else if (imc >= 18.5 && imc < 30){
+            classification = 'Acima do peso'
+        } else if (imc >= 18.5 && imc < 35){
+            classification = 'Obesidade grau I'
+        } else if (imc >= 18.5 && imc < 41){
+            classification = 'Obesidade grau II'
+        } else {
+            classification = 'Obesidade grau III'
+        }
+        resultado.innerHTML = `IMC: ${imc} (${classification})`
+    }else{
+        resultado.innerHTML = 'Preencha os campos'
+    }
+}
